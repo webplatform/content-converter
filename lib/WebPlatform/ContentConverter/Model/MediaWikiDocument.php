d15e6bc906f290fb549f4658305be5270e51069e
Renoir Boulanger <renoir@w3.org>
2015-07-14 14:52:19 -0400

    Mistake in how defining file name and language

--- lib/WebPlatform/ContentConverter/Model/MediaWikiDocument.php
+++ lib/WebPlatform/ContentConverter/Model/MediaWikiDocument.php
@@ -54,6 +54,78 @@ class MediaWikiDocument
     /** @var \SplDoublyLinkedList of MediaWikiRevision objects */
     protected $revisions = array();
 
+    const LANG_ENGLISH = 0;
+
+    const LANG_JAPANESE = 'ja';
+
+    const LANG_GERMAN = 'de';
+
+    const LANG_TURKISH = 'tr';
+
+    const LANG_KOREAN = 'ko';
+
+    const LANG_SPANISH = 'es';
+
+    const LANG_PORTUGUESE_BRAZIL = 'pt-br';
+
+    const LANG_PORTUGUESE = 'pt';
+
+    const LANG_CHINESE = 'zh';
+
+    const LANG_CHINESE_HANT = 'zh-hant';
+
+    const LANG_CHINESE_HANS = 'zh-hans';
+
+    const LANG_FRENCH = 'fr';
+
+    const LANG_SWEDISH = 'sv';
+
+    const LANG_DUTCH = 'nl';
+
+    const LANG_ = '';
+
+    /** String RegEx to find if the page is a page translation */
+    const REGEX_LANGUAGES = '/\/(ar|ast|az|bcc|bg|br|ca|cs|da|de|diq|el|eo|es|fa|fi|fr|gl|gu|he|hu|hy|id|it|ja|ka|kk|km|ko|ksh|kw|mk|ml|mr|ms|nl|no|oc|pl|pt|pt\-br|ro|ru|si|sk|sl|sq|sr|sv|ta|th|tr|uk|vi|yue|zh)$/';
+
+    /**
+     * Commonly used translation codes used in WebPlatform Docs
+     *
+     * Each key represent a language code generally put at the end of a page URL (e.g. Main_Page/es).
+     *
+     * Value is an array of two;
+     * 1. CAPITALIZED english name of the language (e.g. self::$translationCodes['zh'][0] would be 'CHINESE'), so we could map back to self::CHINESE,
+     * 2. Language name in its native form (e.g. self::$translationCodes['zh'][1] would be '中文')
+     *
+     * Should reflect the list of defined translation in [[Template:Languages]] source.
+     *
+     *   - https://docs.webplatform.org/wiki/Template:Languages?action=raw
+     *
+     * See also:
+     *   - https://docs.webplatform.org/w/index.php?title=Special%3AWhatLinksHere&target=Template%3ALanguages&namespace=0
+     *   - https://docs.webplatform.org/wiki/WPD:Translations
+     *   - https://docs.webplatform.org/wiki/WPD:Multilanguage_Support
+     *   - https://docs.webplatform.org/wiki/WPD:Implementation_Patterns
+     *   - http://www.w3.org/International/articles/language-tags/
+     *
+     * @var array
+     */
+    public static $translationCodes = array(
+                    'en'      => ['ENGLISH', 'English'],
+                    'ja'      => ['JAPANESE', '日本語'],
+                    'de'      => ['GERMAN', 'Deutsch'],
+                    'tr'      => ['TURKISH', 'Türkçe'],
+                    'ko'      => ['KOREAN', '한국어'],
+                    'es'      => ['SPANISH', 'Español'],
+                    'pt-br'   => ['PORTUGUESE_BRAZIL', 'Português do Brasil'],
+                    'pt'      => ['PORTUGUESE', 'Português'],
+                    'zh'      => ['CHINESE', '中文'],
+                    'zh-hant' => ['CHINESE_HANT', '中文（繁體）'],
+                    'zh-hans' => ['CHINESE_HANS', '中文（简体）'],
+                    'fr'      => ['FRENCH', 'Français'],
+                    'sv'      => ['SWEDISH', 'Svenska'],
+                    'nl'      => ['DUTCH', 'Nederlands']
+                );
+
     /**
      * Constructs a WikiPage object.
      *
@@ -111,7 +183,7 @@ class MediaWikiDocument
             }
         }
 
-        $fileName = preg_replace('~[\s\:\(\)]+~u', '_', $fileName);
+        $fileName = preg_replace('~[\s\:\!\(\)]+~u', '_', $fileName);
 
         // Remove punctuation
         $fileName = preg_replace('~[\?]+~u', '', $fileName);
@@ -133,6 +205,22 @@ class MediaWikiDocument
         }
 
         return false;
+    }
+
+    public function isTranslation()
+    {
+        $out = null;
+        preg_match(self::REGEX_LANGUAGES, $this->getTitle(), $out);
+
+        return (is_array($out) && isset($out[1]))?true:false;
+    }
+
+    public function getLanguageCode()
+    {
+        $out = null;
+        preg_match(self::REGEX_LANGUAGES, $this->getTitle(), $out);
+
+        return (is_array($out) && isset($out[1])) ? $out[1] : 'en' /* Must match w/ self::$translationCodes['en'] */;
     }
 
     public function getTitle()
